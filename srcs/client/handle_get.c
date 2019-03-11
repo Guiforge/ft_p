7512af20e70b10b0d93a875f009d7d0042d2fafa
Guillaume Pouyat <guiforge@gmail.com>
2019-03-11 19:59:12 +0100

    Mini fix get bin

--- srcs/client/handle_get.c
+++ srcs/client/handle_get.c
@@ -6,11 +6,22 @@
 /*   By: gpouyat <gpouyat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/01 17:05:13 by gpouyat           #+#    #+#             */
-/*   Updated: 2019/03/08 16:18:32 by gpouyat          ###   ########.fr       */
+/*   Updated: 2019/03/11 18:24:23 by gpouyat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/client.h"
+
+static void	static_handle_get_read_write_bin(t_ftp_client *c, int fd)
+{
+	char		line[2048];
+	ssize_t		ret;
+
+	while ((ret = read(c->dtp, line, sizeof(line))) && ret != -1)
+		write(fd, line, ret);
+}
+
+
 
 static void	static_handle_get_read_write(t_ftp_client *c, int fd)
 {
@@ -20,13 +31,8 @@ static void	static_handle_get_read_write(t_ftp_client *c, int fd)
 	line = NULL;
 	while ((ret = get_next_line(c->dtp, &line)) && ret != -1)
 	{
-		if (c->ascii)
-		{
-			ft_overwrite(line, '\r', '\n', -1);
-			ft_putstr_fd(line, fd);
-		}
-		else
-			ft_putendl_fd(line, fd);
+		ft_overwrite(line, '\r', '\n', -1);
+		ft_putstr_fd(line, fd);
 		ft_strdel(&line);
 	}
 	ft_strdel(&line);
@@ -49,7 +55,10 @@ int			handle_get(t_ftp_client *c, char *cmd)
 		ftp_recv(c->sock);
 		return (EXIT_FAILURE);
 	}
-	static_handle_get_read_write(c, fd);
+	if (c->ascii)
+		static_handle_get_read_write(c, fd);
+	else
+		static_handle_get_read_write_bin(c, fd);
 	ftp_recv(c->sock);
 	return (EXIT_SUCCESS);
 }
